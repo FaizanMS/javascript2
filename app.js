5acca41bfb66a8e736fcf98e002e50996c2d492b
Muhammad Faizan Alam <79311662+FaizanMS@users.noreply.github.com>
2023-02-10 00:46:09 +0500

    Assignment 2 Done!

---
+++ app.js
@@ -0,0 +1,76 @@
+
+                                                                                /* Diamond */
+
+// var a, l, u;
+// u = parseInt(prompt("Please enter the length of diamond", "10"));
+// if(!isNaN(u)){
+//     a=u
+//     l=u
+//     for(i=1; i<=l; i++){
+            
+//             for(j=1; j<=a; j++){
+//                 document.write("&nbsp;");
+               
+//             }
+//             a--
+//             for(k=1; k<=i; k++){
+//                 document.write("*");
+//             }
+//             document.write("<br>");
+//     }
+//     a = u;
+//     for(i=1; i<=l; i++){
+
+//             for(k=1; k<=i; k++){
+//                 document.write("&nbsp");
+//             }
+//             for(j=1; j<=a; j++){
+//                 document.write("*");
+//             }
+//             a--
+//             document.write("<br>");
+
+           
+
+//     }
+// }
+// else{
+//     document.write("Invalid input");
+// }
+
+                                                                                /* Number */
+
+// var userInp;
+// userInp = parseInt(prompt("Please enter the length", "10"));
+
+// if(!isNaN(userInp)){
+// for(i=0; i<=userInp; i++){
+//     for(j=i; j>=0; j--){
+//         document.write(j, " ");
+//     }
+//     document.write("<br>")
+// }
+// }
+// else{
+//     document.write("Invalid input!");
+// }
+
+                                                                                /* Stars */
+
+
+// var userInp;
+// userInp = parseInt(prompt("Please enter the length", "10"));
+
+// if(!isNaN(userInp)){
+//     for(i=0; i<userInp; i++){
+//         for(j=0; j<=i; j++){
+//                 document.write("*", "&nbsp;");
+//         }
+//         document.write("<br>")
+//     }
+// }
+// else{
+//     document.write("Invalid input!");
+// }                                                                                
+
+
